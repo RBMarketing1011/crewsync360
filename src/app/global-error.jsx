3c7c310c6c2f851962d0b1a45ece2a3625a9ee3d
Anthony Reynolds <RBMarketingAndAnalytics@gmail.com>
2024-10-10 19:24:29 -0400

    update .env in vercel

--- src/app/global-error.jsx
+++ src/app/global-error.jsx
@@ -14,7 +14,6 @@ export default function GlobalError ({ error })
   return (
     <html>
       <body>
-        {/* Your Error component here... */ }
         {/* `NextError` is the default Next.js error page component. Its type
         definition requires a `statusCode` prop. However, since the App Router
         does not expose status codes for errors, we simply pass 0 to render a
