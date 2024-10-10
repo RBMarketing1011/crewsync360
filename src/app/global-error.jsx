50f82a7123da3c1a392ea2364b2894b71f55d10e
Anthony Reynolds <RBMarketingAndAnalytics@gmail.com>
2024-10-10 18:54:36 -0400

    fix Global Error page

--- src/app/global-error.jsx
+++ src/app/global-error.jsx
@@ -19,7 +19,7 @@ export default function GlobalError ({ error })
         definition requires a `statusCode` prop. However, since the App Router
         does not expose status codes for errors, we simply pass 0 to render a
         generic error message. */}
-        <NextError statusCode={ 0 } />
+        <Error statusCode={ 0 } />
       </body>
     </html>
   )
